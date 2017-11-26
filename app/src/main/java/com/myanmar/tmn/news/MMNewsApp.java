18d3a5fd19ec2fc8593683bfcc3e12f4220f2c9a
Thet Myat Noe <thetmyatnoe223@gmail.com>
2017-11-26 15:35:48 +0630

    Bind View

---
+++ app/src/main/java/com/myanmar/tmn/news/MMNewsApp.java
@@ -0,0 +1,10 @@
+package com.myanmar.tmn.news;
+
+import android.app.Application;
+
+/**
+ * Created by msi on 11/26/2017.
+ */
+
+public class MMNewsApp extends Application {
+}
--- app/src/main/java/com/myanmar/tmn/news/activities/MainActivity.java
+++ app/src/main/java/com/myanmar/tmn/news/activities/MainActivity.java
@@ -1,31 +1,40 @@
-package com.myanmar.tmn.padc_3_f_tmn_mm_news;
+package com.myanmar.tmn.news.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.myanmar.tmn.news.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.rv_news)
+    RecyclerView rvNews;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ButterKnife.bind(this,this);
+
+        setSupportActionBar(toolbar);
     }
 
     @Override
@@ -48,5 +57,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.fab)
+    public void onClickFab(View view){
+        Snackbar.make(view, "Replace with your own action - Butterknife", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 }
